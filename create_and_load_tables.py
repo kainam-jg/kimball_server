3120eca0fd7c6a44bf7e8a70db647d0e39fac6ec
Justin Gillespie <jgillespie@kainam.ai>
2025-04-05 08:31:38 -0400

    Add session-token handling and ClickHouse logging

--- create_and_load_tables.py
+++ create_and_load_tables.py
@@ -93,7 +93,7 @@ async def create_and_load_tables(data: TableData, auth: bool = Depends(verify_au
             ALTER TABLE default.file_upload_log 
             UPDATE 
                 end_time = toDateTime('{end_time}'),
-                table_names = {tables}
+                table_names = {tables},
                 file_names = {filenames}
             WHERE session_token = '{session_token}'
         """
