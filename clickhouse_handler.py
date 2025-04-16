8a65ee7e656b15e0eecb65abda81482f58e65253
Justin Gillespie <jgillespie@kainam.ai>
2025-04-16 09:57:40 -0400

    Updated clickhouse handler

--- clickhouse_handler.py
+++ clickhouse_handler.py
@@ -34,19 +34,13 @@ SCHEME = "https" if CH_SSL else "http"
 BASE_URL = f"{SCHEME}://{CH_HOST}:{CH_PORT}/"
 
 CLIENT_FLAGS = ["clickhouse-client"]
-if CH_SSL:
-    CLIENT_FLAGS += ["--secure"]
-    if CH_SSL_CERT:
-        CLIENT_FLAGS += ["--ssl-cert", CH_SSL_CERT]
-        CLIENT_FLAGS += ["--ssl-verify=0"]
-    CLIENT_FLAGS += ["--port", "9440"]
-else:
-    CLIENT_FLAGS += ["--port", str(CH_PORT)]
-CLIENT_FLAGS += ["--host", CH_HOST]
 if CH_USER:
     CLIENT_FLAGS += ["--user", CH_USER]
 if CH_PASS:
-    CLIENT_FLAGS += ["--password", CH_PASS]
+    CLIENT_FLAGS += ["--password", f"'{CH_PASS}'"]
+if CH_SSL:
+    CLIENT_FLAGS += ["--port", "9440", "--secure"]
+CLIENT_FLAGS += ["--host", CH_HOST]
 
 # Define all functions
 def run_clickhouse_query(query: str):
